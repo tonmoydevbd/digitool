a679880b3700ddc22a6034d16392a9772d2c2c0c
tonmoydevbd <tonmoydevbd@gmail.com>
2026-06-01 18:31:01 +0600

    navbar added.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { FiShoppingCart } from "react-icons/fi";
+
+export const Navbar = () => {
+  const navLink = ['products', 'features', 'pricing', 'testimonials', 'faq'];
+  const navLinkElements = navLink.map(item => <li key={item}><a href="/">{item.trim().toUpperCase()}</a></li>)
+
+
+  return (
+
+    <div className=" bg-base-100 shadow-sm ">
+      <div className="navbar max-w-300 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              {navLinkElements}
+            </ul>
+          </div>
+          <a className="bg-linear-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-transparent font-bold text-2xl btn btn-ghost">DigiTools</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {navLinkElements}
+          </ul>
+        </div>
+        <div className="navbar-end flex gap-4">
+          <FiShoppingCart className="font-bold text-xl" />
+          <a>Login</a>
+          <a className="btn bg-linear-to-r from-pink-500 via-purple-500 to-indigo-500 text-white font-semibold py-2.5 px-6 rounded-full transition-all duration-300  hover:scale-105 shadow-md">Get Started</a>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Navbar } from './components/Navbar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <p>starting...</p>
+    <Navbar />
   </StrictMode>,
 )
