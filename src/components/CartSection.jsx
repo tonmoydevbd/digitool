b1330b163a6199c763a8966f9170b3fa28dc7836
tonmoydevbd <tonmoydevbd@gmail.com>
2026-06-04 22:04:04 +0600

    wrote better code for cart in cart section.

--- src/components/CartSection.jsx
+++ src/components/CartSection.jsx
@@ -17,24 +17,23 @@ export const CartSection = (props) => {
     return price
   }
 
-  const cartProductEl = cartProduct.length === 0
-    ? <h3 className="font-bold text-xl">No product in cart.</h3>
-    : cartProduct.map(item => <div key={item.id} className="my-6 flex flex-col gap-4 ">
-      <div className="flex flex-col sm:flex-row gap-4 bg-gray-200 rounded-xl items-center p-4">
-        <img className="rounded-full p-4 bg-white" src={item.icon} alt="" />
-        <div className="grow text-start">
-          <h4 className="font-semibold text-xl">{item.name}</h4>
-          <p>${item.price}</p>
-        </div>
-        <button onClick={() => { handleRemoveProduct(item.id); notify(item.name) }} className="btn btn-ghost text-red-800">Remove</button>
-      </div>
-    </div>)
+  const cartProductEl = cartProduct.map(item => <div key={item.id} className="flex flex-col sm:flex-row gap-4 bg-gray-200 rounded-xl items-center p-4">
+    <img className="rounded-full p-4 bg-white" src={item.icon} alt="" />
+    <div className="grow text-start">
+      <h4 className="font-semibold text-xl">{item.name}</h4>
+      <p>${item.price}</p>
+    </div>
+    <button onClick={() => { handleRemoveProduct(item.id); notify(item.name) }} className="btn btn-ghost text-red-800">Remove</button>
+  </div>)
+
 
 
   return (
     <div className="max-w-300 mx-auto mt-10 p-4 sm:p-10 border border-gray-300 rounded-xl">
       <h3 className="text-2xl font-bold text-start">Your Cart</h3>
-      {cartProductEl}
+      {cartProduct.length === 0
+        ? <h3 className="font-bold text-xl">No product in cart.</h3>
+        : <div className="my-6 flex flex-col gap-4 ">{cartProductEl}</div>}
       <div className="flex justify-between items-center my-6">
         <p>Total</p>
         <p className="text-2xl font-bold">${totalPrice()}</p>
